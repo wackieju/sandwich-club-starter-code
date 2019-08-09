8e2e26219a3eb66f47a67d45877bdf7e51271b39
wackieju <jiajie.wu96@gmail.com>
2019-08-08 19:45:19 -0700

    Updated JsonUtils to query the JSON data

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,47 @@
 package com.udacity.sandwichclub.utils;
 
+import android.text.TextUtils;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        if (TextUtils.isEmpty(json)) {
+            return null;
+        }
+        try {
+            JSONObject root = new JSONObject(json);
+            JSONArray nameOccurences = root.getJSONArray("name");
+            String sandwichName = "";
+            List<String> akaList = new ArrayList<>();
+            for (int i = 0; i < nameOccurences.length(); i++) {
+                JSONObject nameObject = nameOccurences.getJSONObject(i);
+                sandwichName = nameObject.getString("mainName");
+                JSONArray akaArray = nameObject.getJSONArray("alsoKnownAs");
+                for (int j = 0; j < akaArray.length(); j++) {
+                    akaList.add(akaArray.getString(i));
+                }
+            }
+            String placeOfOrigin = root.getString("placeOfOrigin");
+            String description = root.getString("description");
+            String image = root.getString("image");
+            JSONArray ingredientsArray = root.getJSONArray("ingredients");
+            List<String> ingredients = new ArrayList<>();
+            for (int i = 0; i < ingredientsArray.length(); i++) {
+                ingredients.add(ingredientsArray.getString(i));
+            }
+            return new Sandwich(sandwichName, akaList, placeOfOrigin, description, image, ingredients);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return new Sandwich();
     }
 }
